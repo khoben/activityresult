522618e5a931de2157a77612721e7bda7ea2c06e
khoben <extless@gmail.com>
2021-10-08 12:43:50 +0700

    Update to 2.0.0-alpha03
    - PermissionRequest: register launcher when register() called
    - PermissionRequest: check valid registration and launching states
    - Update dependencies
    - Update sample app with using 2.0.0-alpha03 version
    - Bump version to 2.0.0-alpha03

--- arpermission/src/main/java/io/github/khoben/arpermission/PermissionRequest.kt
+++ arpermission/src/main/java/io/github/khoben/arpermission/PermissionRequest.kt
@@ -3,6 +3,7 @@ package io.github.khoben.arpermission
 import androidx.activity.ComponentActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import io.github.khoben.arpermission.permission.PermissionRequestLauncher
 import io.github.khoben.arpermission.permission.registerPermissions
@@ -15,13 +16,29 @@ class PermissionRequest : DefaultLifecycleObserver {
     private var onDenied: ((permissions: List<String>, isCancelled: Boolean) -> Unit)? = null
     private var onExplained: ((permissions: List<String>) -> Unit)? = null
 
+    /**
+     * Register а permissions request.
+     *
+     * Must be called before [lifecycleOwner] reach [Lifecycle.State.STARTED] state.
+     *
+     * @param lifecycleOwner LifecycleOwner for which the permission request will be registered.
+     * An instance of [ComponentActivity] or [Fragment] is expected.
+     */
     fun register(lifecycleOwner: LifecycleOwner) {
-        lifecycleOwner.lifecycle.addObserver(this)
-    }
 
-    override fun onCreate(owner: LifecycleOwner) {
-        if (owner is ComponentActivity) {
-            launcher = owner.registerPermissions {
+        val lifecycle = lifecycleOwner.lifecycle
+
+        check(!lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
+            ("LifecycleOwner " + lifecycleOwner + " is "
+                    + "attempting to register while current state is "
+                    + lifecycle.currentState + ". LifecycleOwners must call register before "
+                    + "they are STARTED.")
+        }
+
+        lifecycle.addObserver(this)
+
+        if (lifecycleOwner is ComponentActivity) {
+            launcher = lifecycleOwner.registerPermissions {
                 allGranted = {
                     onGranted?.invoke()
                 }
@@ -37,8 +54,8 @@ class PermissionRequest : DefaultLifecycleObserver {
                     onDenied = null
                 }
             }
-        } else if (owner is Fragment) {
-            launcher = owner.registerPermissions {
+        } else if (lifecycleOwner is Fragment) {
+            launcher = lifecycleOwner.registerPermissions {
                 allGranted = {
                     onGranted?.invoke()
                 }
@@ -57,17 +74,29 @@ class PermissionRequest : DefaultLifecycleObserver {
         }
     }
 
-    override fun onDestroy(owner: LifecycleOwner) {
+    override fun onDestroy(lifecycleOwner: LifecycleOwner) {
         launcher.unregister()
-        owner.lifecycle.removeObserver(this)
+        lifecycleOwner.lifecycle.removeObserver(this)
     }
 
+    /**
+     * Launch a [permissions] request
+     *
+     * @param permissions Requested permissions
+     * @param onDenied callback: list of denied permissions, also [isCancelled] indicates
+     * whether the request was canceled
+     * @param onExplained callback: list of permissions that should be explained
+     * @param onGranted callback: all requested [permissions] are granted
+     */
     fun launch(
         vararg permissions: String,
         onDenied: ((permissions: List<String>, isCancelled: Boolean) -> Unit)? = null,
         onExplained: ((permissions: List<String>) -> Unit)? = null,
         onGranted: (() -> Unit)? = null,
     ) {
+        check(::launcher.isInitialized) {
+            ("The permission request has not been registered before.")
+        }
         this.onDenied = onDenied
         this.onExplained = onExplained
         this.onGranted = onGranted
